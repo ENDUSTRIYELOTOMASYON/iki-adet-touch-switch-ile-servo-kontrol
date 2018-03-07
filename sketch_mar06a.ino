6564c23e23a81492483be79ba95f3bbc0866dbf8
AKIN KAHRAMAN <36744498+ENDUSTRIYELOTOMASYON@users.noreply.github.com>
2018-03-07 08:13:17 +0200

    Add files via upload

---
+++ sketch_mar06a.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+int servo_aci_degeri=0;
+Servo servo_kontrol;
+#define servo_pin 11
+#define buton_yukari 3
+#define buton_asagi 2
+void setup() {
+  servo_kontrol.attach(servo_pin);
+  pinMode(buton_yukari,INPUT);
+  pinMode(buton_asagi,INPUT);
+}
+void loop() {
+  if (digitalRead(buton_yukari)==HIGH)
+  servo_aci_degeri++;
+  if (digitalRead(buton_asagi)==HIGH)
+  servo_aci_degeri--;
+
+  if(servo_aci_degeri>180)
+  servo_aci_degeri=180;
+
+  if(servo_aci_degeri<0)
+  servo_aci_degeri=0;
+  servo_kontrol.write(servo_aci_degeri);
+  delay(20);
+}
